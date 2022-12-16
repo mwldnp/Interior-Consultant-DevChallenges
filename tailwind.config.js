1d73008a1a53875e9d91a55546c711a6a1bccf57
Wildan132 <mwp113322758@gmail.com>
2022-12-16 23:12:11 +0700

    Upload project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Montserrat' : 'Montserrat',
+        'Lora' : 'Lora',
+        'Crimson-Pro': 'Crimson Pro'
+      },
+
+      colors: {
+        primary: '#181719',
+      }
+    },
+  },
+  plugins: [],
+}
